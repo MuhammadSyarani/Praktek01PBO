9bf9ad938de4b0f3e93a03fabbf49af95dfdfbe2
MuhammadSyarani <32213485+MuhammadSyarani@users.noreply.github.com>
2018-04-05 20:29:30 +0800

    Mengisi nilai atribut

--- src/praktek01pbo/PohonBerbuah.java
+++ src/praktek01pbo/PohonBerbuah.java
@@ -14,5 +14,25 @@ public class PohonBerbuah {
         PohonBuah Kelapa = new PohonBuah();
         PohonBuah Mangga = new PohonBuah();
         PohonBuah Nangka = new PohonBuah();
+        
+        Kelapa.namapohon="Pohon Kelapa";
+        Kelapa.bentukbuah="Bulat";
+        Kelapa.bentukdaun="Panjang";
+        Kelapa.jenisakar="Serabut";
+        Kelapa.perkembangan="3 meter";
+        
+        Mangga.namapohon="Pohon Mangga";
+        Mangga.bentukbuah="Lonjong";
+        Mangga.bentukdaun="Panjang";
+        Mangga.jenisakar="Tunggang";
+        Mangga.perkembangan="1.5 meter";
+        
+        Nangka.namapohon="Pohon Nangka";
+        Nangka.bentukbuah="Bulat Berduri";
+        Nangka.bentukdaun="Lebar";
+        Nangka.jenisakar="Tunggang";
+        Nangka.perkembangan="2 meter";
+        
+        
     }
 }
